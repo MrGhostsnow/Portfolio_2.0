a8e853b7c71f41b614fa0e4f91711023260376aa
unknown <victorio5054@gmail.com>
2022-06-20 15:07:33 -0300

    buttons about

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelector('.btns_Direcao');
+const sobreMim = document.querySelector('.btn_SobreMim');
+const projetos = document.querySelector('.btn_Projetos');
+const habilidades = document.querySelector('.btn_Habilidades');
+const contato = document.querySelector('.btn_Contato');
+
+
+
+
+function showBtns() {
+    sobreMim.style.display= "flex"
+    sobreMim.style.opacity = "1"
+    projetos.style.opacity = "1"
+    projetos.style.display= "flex"
+    habilidades.style.opacity = "1"
+    habilidades.style.display= "flex"
+    contato.style.opacity = "1"
+    contato.style.display= "flex"
+    
+}
+
