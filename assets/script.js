944b70a8fb3f6f3629a48a10e0c6ad7eca3dedad
unknown <victorio5054@gmail.com>
2022-06-26 15:42:59 -0300

    sobre mim

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,47 @@
-const buttons = document.querySelector('.btns_Direcao');
-const btnsobreMim = document.querySelector('.btn_SobreMim');
-const btnprojetos = document.querySelector('.btn_Projetos');
-const btnhabilidades = document.querySelector('.btn_Habilidades');
-const btncontato = document.querySelector('.btn_Contato');
+const buttons = document.querySelector(".btns_Direcao");
+const btnsobreMim = document.querySelector(".btn_SobreMim");
+const btnprojetos = document.querySelector(".btn_Projetos");
+const btnhabilidades = document.querySelector(".btn_Habilidades");
+const btncontato = document.querySelector(".btn_Contato");
 const btnvoltar = document.querySelector("#back_arrow");
-const firstCard = document.querySelector('.card_FirstPage')
-const containerAbouMe = document.querySelector('.container_SobreMim');
-const containerProjects = document.querySelector('.container_projetos');
-const containerSkills = document.querySelector('.card_habilidades')
-const ouroboros = document.getElementById("logo")
+const firstCard = document.querySelector(".card_FirstPage");
+const containerAbouMe = document.querySelector(".container_SobreMim");
+const containerProjects = document.querySelector(".container_projetos");
+const containerSkills = document.querySelector(".card_habilidades");
+const ouroboros = document.getElementById("logo");
 
 function showBtns() {
-    btnsobreMim.style.display= "flex"
-    btnsobreMim.style.opacity = "1"
-    btnprojetos.style.opacity = "1"
-    btnprojetos.style.display= "flex"
-    btnhabilidades.style.opacity = "1"
-    btnhabilidades.style.display= "flex"
-    btncontato.style.opacity = "1"
-    btncontato.style.display= "flex"
-    
+  btnsobreMim.style.display = "flex";
+  btnsobreMim.style.opacity = "1";
+  btnprojetos.style.opacity = "1";
+  btnprojetos.style.display = "flex";
+  btnhabilidades.style.opacity = "1";
+  btnhabilidades.style.display = "flex";
+  btncontato.style.opacity = "1";
+  btncontato.style.display = "flex";
 }
 
 function showAboutMe() {
-    addEventListener("click", () => {
-        firstCard.style.display = "none";
-        containerAbouMe.style.display = "flex";
-        containerSkills.style.display = "none"
-
-    })
+  addEventListener("click", () => {
+    firstCard.style.display = "none";
+    containerAbouMe.style.display = "flex";
+    containerSkills.style.display = "none";
+  });
 }
 
 function showProjects() {
-    addEventListener("click", () => {
-        firstCard.style.display = "none";
-        containerProjects.style.display = "flex";
-        containerSkills.style.display = "none"
-    })
+  addEventListener("click", () => {
+    firstCard.style.display = "none";
+    containerProjects.style.display = "flex";
+    containerSkills.style.display = "none";
+  });
 }
 
 function backToFirstPage() {
-    addEventListener("click", () => {
-        firstCard.style.display = "flex";
-        containerAbouMe.style.display = "none";
-        containerProjects.style.display = "none";
-        containerSkills.style.display = "flex"
-    })
+  addEventListener("click", () => {
+    firstCard.style.display = "flex";
+    containerAbouMe.style.display = "none";
+    containerProjects.style.display = "none";
+    containerSkills.style.display = "flex";
+  });
 }
-
